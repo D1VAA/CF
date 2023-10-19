97153dabdf1ce1a77246440cbe60f6731efc0280
HyBun <davipare203@gmail.com>
2023-10-19 01:42:01 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#0a0613',
+        secondary: '#401d5a',
+        third: '#490e7d'
+      }
     },
   },
   plugins: [],
